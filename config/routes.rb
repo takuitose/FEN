e741577e1460418714b449d2f86f506b90bb45bd
takuitose <132122425+takuitose@users.noreply.github.com>
2023-07-06 12:05:17 +0000

    [Add]TOP&ABOUT

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-# 顧客用
-# URL /members/sign_in ...
-devise_for :members, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
+
+  # 顧客用
+  # URL /members/sign_in ...
+  devise_for :members, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+    root 'homes#top'
+    get  '/about' => 'homes#about'
+  end
 
 
 end
