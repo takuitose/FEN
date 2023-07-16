e059730ffbf1563e50cd45192a415d87338b8aa7
takuitose <132122425+takuitose@users.noreply.github.com>
2023-07-16 17:49:41 +0000

    [fix]registration

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
+    sessions: 'admin/sessions',
   }
 
   namespace :admin do
@@ -15,8 +15,8 @@ Rails.application.routes.draw do
   # 顧客用
   # URL /members/sign_in ...
   devise_for :members, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
+    registrations: 'public/registrations',
+    sessions: 'public/sessions',
   }
   # guest用
   devise_scope :member do
