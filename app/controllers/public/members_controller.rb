bec2f841820268d1a5907f113e50b34ae02203a8
takuitose <132122425+takuitose@users.noreply.github.com>
2023-07-26 03:14:32 +0000

    [update]member

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,15 +1,26 @@
 class Public::MembersController < ApplicationController
 
   def show
-
+    @spots = Spot.order(id: :desc).limit(3)
   end
 
   def edit
-
+    @member = current_member
   end
 
   def update
+    @member = current_member
+    if @member.update(member_params)
+      redirect_to mypage_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email)
   end
 
 end
